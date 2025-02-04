7e866e7520842ea26f7b216f95ec39041b64c206
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-02-04 15:09:21 +0530

    debouncing input

---
+++ src/Debouncing/DebouncingInput.tsx
@@ -0,0 +1,62 @@
+import React ,{useState,useRef,useEffect} from 'react'
+import './Debouncing.css'
+
+
+
+const DebouncingInput=()=>{
+
+    const ref=useRef<HTMLElement>(null)
+    const [input,setInput]=useState<string>("")
+
+    const handlechange=(event:any)=>{
+        setInput(event.target.value)
+        console.log(input)
+        if(ref.current){
+            ref.current.value=input;
+        }
+    }
+
+    const debouncingfunc=(func:any,delay:any)=>{
+        let timer;
+
+        return (...args)=>{
+            clearTimeout(timer)
+            timer=setTimeout(()=>{
+                func(...args)
+            },delay)
+        }
+
+    }
+
+    const debouncinghandle=debouncingfunc(handlechange,3000)
+
+
+    return (
+        <div className="mainbody">
+            <div className="divclass">
+                <div>
+                    <strong>Enter the value</strong>
+                </div>
+                <input 
+                    type="text" 
+                    placeHolder="Enter the value "
+                    style={{padding:"1rem"}}
+                    onChange={debouncinghandle}
+                 />
+            </div>
+            <div className="divclass">
+                <div>
+                    <strong>Entered Value</strong>
+                </div>
+                <input type="text" 
+                placeholder="The value is"
+                ref={ref}
+                style={{padding:"1rem"}}
+                value={input}
+            />
+            </div>
+        </div>
+    )
+}
+
+export default DebouncingInput
